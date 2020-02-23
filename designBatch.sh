0727617f7a620d42bd0ca0c3d3f35367a9bf0c82
Leah <lweber21@gmail.com>
2020-02-23 16:52:00 -0600

    restructuring directory

--- designBatch.sh
+++ designBatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir distFeats
-for f in ./*; do
+for f in ./*t; do
     #open the file and get the number of trees
     echo $f
     line=$(sed -n '2{p;q}' $f)
@@ -9,9 +9,9 @@ for f in ./*; do
     NUMBER=$(echo $line | grep -o -E '[0-9]+')
   
 
-    
+    let NUMBER-=1
     #loop through for each tree and find the the distinguishing features
-    until [  $NUMBER -lt 1 ]; do
+    until [  $NUMBER -lt 0 ]; do
              echo COUNTER $NUMBER
              design $f -i $NUMBER
              let NUMBER-=1
