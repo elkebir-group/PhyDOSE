4fbde2996260c29df4a37d447daea5cd5a8eb390
Leah <lweber21@gmail.com>
2020-02-24 13:06:54 -0600

    adding bash script for testing on mac

---
+++ designBatch_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir distFeats
+for f in ./*t; do
+    #open the file and get the number of trees
+    echo $f
+    line=$(sed -n 2p "$f")
+    echo $line
+    NUMBER=$(echo $line | grep -o -E '[0-9]+')
+  
+
+    let NUMBER-=1
+    #loop through for each tree and find the the distinguishing features
+    until [  $NUMBER -lt 0 ]; do
+             echo COUNTER $NUMBER
+             design $f -i $NUMBER
+             let NUMBER-=1
+    done
+done
